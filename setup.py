a0c89c75ea44082050c5acacc682c18614e89ac8
msempere <odoncopon@gmail.com>
2014-12-16 15:08:29 +0000

    main setup config file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='easy_cache',
+      version='0.1',
+      description='Easy memory cache for single-threaded environments',
+      url='https://github.com/msempere/easy_cache',
+      author='msempere',
+      author_email='msempere@users.noreply.github.com',
+      license='MIT',
+      packages=['easy_cache'],
+      zip_safe=False)
