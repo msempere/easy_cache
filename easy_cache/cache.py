fb5c93aea57cabdd0bc5a8b9daa091195af5bbe8
msempere <odoncopon@gmail.com>
2014-12-16 19:07:00 +0000

    Add LFU, RR and MRU algorithms

--- easy_cache/cache.py
+++ easy_cache/cache.py
@@ -1,11 +1,13 @@
 from time import time
+from random import randrange
 
+HITS_DEFAULT = 0
 
 class Algorith(object):
     LRU = 1
-    # TODO MRU = 2
-    # TODO RR  = 3
-    # TODO LFU = 4
+    MRU = 2
+    RR  = 3
+    LFU = 4
 
 
 class EasyCache(object):
@@ -25,11 +27,22 @@ class EasyCache(object):
         if len(self._cache) > self.capacity:
             num_items_to_remove = len(self._cache) - self.capacity
 
-            if self.algorith == Algorith.LRU:
-                sorted_cache = sorted(self._cache.items(), key=lambda x: x[1][3])
+            if self.algorith == Algorith.RR:
+                for item in range(0 , num_items_to_remove - 1):
+                    self.remove(self._cache.items()[0][randrange(0,len(self.cache) - 1)])
+            else:
+                if self.algorith == Algorith.LRU:
+                    sorted_cache = sorted(self._cache.items(), key=lambda x: x[1][3])
+
+                elif self.algorith == Algorith.MRU:
+                    sorted_cache = sorted(self._cache.items(), key=lambda x: x[1][3], reverse=True)
+
+                elif self.algorith == Algorith.LFU:
+                    sorted_cache = sorted(self._cache.items(), key=lambda x: x[1][2])
 
                 for item in range(0 , num_items_to_remove - 1):
                     self.remove(sorted_cache.items()[0][0])
+
 
     def get(self, key, default=None):
         try:
@@ -53,7 +66,7 @@ class EasyCache(object):
                 _eviction, _value, _hits, _used = self._cache[key]
                 self._cache[key] = (_eviction, value, _hits + 1, time())
             else:
-                self._cache[key] = (time() + timeout, value, 0, time() + timeout)
+                self._cache[key] = (time() + timeout, value, HITS_DEFAULT, time() + timeout)
                 self._purgue()
             return True
         except:
